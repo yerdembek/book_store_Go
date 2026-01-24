19331d9edf12ba18df5d06c5692b03ee88c589c9
danialmarat <doni.danialmarat20066@gmail.com>
2026-01-25 00:18:16 +0500

    add mongoDB and Docker with using repo

--- cmd/main.go
+++ cmd/main.go
@@ -2,25 +2,46 @@ package main
 
 import (
 	"book_store_Go/internal/auth"
-	"book_store_Go/internal/mock"
+	"book_store_Go/internal/repository"
+	"context"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	userRepo := mock.NewMockUserRepo()
+	clientOptions := options.Client().ApplyURI("mongodb://admin:password@localhost:27017")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Connection failed:", err)
+	}
+	log.Println("Connected to MongoDB!")
+
+	db := client.Database("bookstore")
+
+	userRepo := repository.NewUserRepository(db)
 
 	authHandler := auth.NewAuthHandler(userRepo)
 
 	r := mux.NewRouter()
 	r.HandleFunc("/register", authHandler.HandleRegister).Methods("POST")
 	r.HandleFunc("/login", authHandler.HandleLogin).Methods("POST")
-
 	r.HandleFunc("/users/me", authHandler.HandleGetMe).Methods("GET")
 	r.HandleFunc("/users/{id}", authHandler.HandleGetUserByID).Methods("GET")
 
-	log.Println("🚀 Server running on http://localhost:8080")
+	log.Println(" Server running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
