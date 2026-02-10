4a7276b71556db4dfa95b14fe0ce3d0d6565b5a7
danialmarat <doni.danialmarat20066@gmail.com>
2026-02-11 00:46:08 +0500

    feat(chat): add Message struct model

---
+++ internal/models/chat.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Message struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Sender    string             `bson:"sender" json:"sender"`
+	Content   string             `bson:"content" json:"content"`
+	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
+}
